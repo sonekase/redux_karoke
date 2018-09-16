41a3689be5e78a60754d9101f0f031d6cc4f7cdc
Anousone Kaseumsouk <anousonekaseumsouk@gmail.com>
2018-09-16 15:45:25 -0700

    Add subscribe to Redux store to display lyrics.

--- scripts.js
+++ scripts.js
@@ -11,7 +11,7 @@ const initialState = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'NEXT_LYRIC':
-      let newArrayPosition = state.arrayPosition +1;
+      let newArrayPosition = state.arrayPosition + 1;
       let newState = {
         songLyricsArray: state.songLyricsArray,
         arrayPosition: newArrayPosition,
@@ -35,9 +35,27 @@ expect(reducer(initialState, { type: 'NEXT_LYRIC' })).toEqual({
 // REDUX STORE
 const { createStore } = Redux;
 const store = createStore(reducer);
-console.log(store.getState());
+
+// RENDERING STATE IN DOM
+const renderLyrics = () => {
+  const lyricsDisplay = document.getElementById('lyrics');
+  while (lyricsDisplay.firstChild) {
+    lyricsDisplay.removeChild(lyricsDisplay.firstChild);
+  }
+  const currentLine = store.getState().songLyricsArray[store.getState().arrayPosition];
+  const renderedLine = document.createTextNode(currentLine);
+  document.getElementById('lyrics').appendChild(renderedLine);
+}
+
+window.onload = function() {
+  renderLyrics();
+}
 
 // CLICK LISTENER
 const userClick = () => {
-  console.log('click');
+  store.dispatch({ type: 'NEXT_LYRIC' });
+  console.log(store.getState());
 }
+
+// SUBSCRIBE TO REDUX STORE
+store.subscribe(renderLyrics);
